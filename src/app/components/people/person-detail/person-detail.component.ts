05014232636c5d0de3f1cedba9e7cabaf2e0d375
Eduard Efren Arango lopez <eduard.arango@correounivalle.edu.co>
2018-08-03 18:23:48 -0500

    Service Pack 03/08/2018: Improved person detail responsive. Added icons for actor birthday, place of birth and deathday. Added person age.

--- src/app/components/people/person-detail/person-detail.component.ts
+++ src/app/components/people/person-detail/person-detail.component.ts
@@ -34,4 +34,16 @@ export class PersonDetailComponent implements OnInit {
     )
   }
 
+  calculateAge():number{
+    let age = 0;
+    if(this.person.deathday){
+      let birthday = +new Date(this.person.birthday);
+      let deathday = +new Date(this.person.deathday);
+      age = ~~((deathday - birthday) / (31557600000));
+    }else{
+      let birthday = +new Date(this.person.birthday);
+      age = ~~((Date.now() - birthday) / (31557600000));
+    }
+    return age;
+  }
 }
